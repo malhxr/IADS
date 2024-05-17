03f7ed043b9773b67dae8e070eaf4b1627eb6633
Akshar Patel <88324261+akshar2223@users.noreply.github.com>
2024-05-17 13:15:06 -0400

    Update part3.py

--- lab2/part3.py
+++ lab2/part3.py
@@ -1,4 +1,8 @@
+#loop through rows from 0 to 6
 for i in range(7):
+    #loop through columns from 0 to 6
     for j in range(7):
+        #check if row index equals column index
         if i == j:
+            #print dollar sign, spaces(equal to row index), another dollar sign
             print("$"+i*" "+"$")
