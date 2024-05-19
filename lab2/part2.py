cab66f76e82898409aeb63b28e12fb6f9f538820
Harsh Sachala <sach46229@gmail.com>
2024-05-19 12:39:16 -0400

    Bug fixes for duplicate contacts and input

--- lab2/part2.py
+++ lab2/part2.py
@@ -1,34 +1,44 @@
 def add_contact(address_book, file_name):
     name = input("Enter name: ")
     email = input("Enter email: ")
-    address_book[name] = email
-    with open(file_name, 'w') as f:
-        for name,email in address_book.items():
-            f.write(f"{name},{email}\n")
-    print("Contact added successfully")
+    # Check for duplicates first
+    if any(contact['name'] == name and contact['email'] == email for contact in address_book):
+        print(f"\nContact {name} with email {email} already exists.")
+    else:
+        address_book.append({'name':name,'email':email})
+        with open(file_name, 'w') as f:
+            for contact in address_book:
+                f.write(f"{contact['name']},{contact['email']}\n")
+        print("\nContact added successfully")
 
 def search_contact(address_book):
-    name = input("Enter name to search: ")
-    if name in address_book:
-        print(f"Email for {name} is {address_book[name]}.")
+    if len(address_book) == 0:
+        print("\nNo contacts.\n")
     else:
-        print(f"Contact not found.")
+        name = input("Enter name to search: ")
+        count = 0
+        for contact in address_book:
+            if name == contact['name']:
+                count += 1
+                print(f"\nEmail for {name} is {contact['email']}.")
+        if count == 0:
+            print(f"\nContact not found.")
 
 def list_all_contacts(address_book):
     if len(address_book) == 0:
         print("\nNo Contacts")
     else:
         print("\nAll Contacts")
-        for name,email in address_book.items():
-            print(f"{name} - {email}")
+        for contact in address_book:
+            print(f"{contact['name']} - {contact['email']}")
 
 def load_address_book(file_name):
-    address_book = {}
+    address_book = []
     try:
         with open(file_name, 'r') as f:
             for line in f:
                 name, email = line.strip().split(',')
-                address_book[name] = email
+                address_book.append({'name':name,'email':email})
     except FileNotFoundError:
         with open(file_name, 'w') as f:
             pass
@@ -45,20 +55,24 @@ def main():
         print("3. List all contacts")
         print("4. Quit")
 
-        user_input = int(input("Enter your choice: "))
+        try:
+            user_input = int(input("Enter your choice: "))
+            if user_input == 1:
+                add_contact(address_book, file_name)
+            elif user_input == 2:
+                search_contact(address_book)
+            elif user_input == 3:
+                list_all_contacts(address_book)
+            elif user_input == 4:
+                print("\nThank you!")
+                print("Exiting.")
+                break
+            else:
+                print("\nInvalid input: Enter valid number from 1 to 4.")
+        except ValueError:
+            print("\nInvalid input: Enter valid number from 1 to 4.")
 
-        if user_input == 1:
-            add_contact(address_book, file_name)
-        elif user_input == 2:
-            search_contact(address_book)
-        elif user_input == 3:
-            list_all_contacts(address_book)
-        elif user_input == 4:
-            print("Thank you!")
-            print("Exiting.")
-            break
-        else:
-            print("Invalid input.")
+        
 
 
 if __name__ == "__main__":
